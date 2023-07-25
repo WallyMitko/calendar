129c6d9fb0600bbb67aa0cd1cc209f04ac0b86f5
WallyMitko <cam@bourdeau.dev>
2023-07-25 13:28:59 -0400

    Create basic backend

--- calendar-frontend/src/main.tsx
+++ calendar-frontend/src/main.tsx
@@ -5,23 +5,23 @@ import App from './App.tsx'
 import './index.css'
 
 const router = createBrowserRouter([
-  // Match 0, 1, or 2 parameters
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/:year",
-    element: <App />
-  },
-  {
-    path: "/:year/:month",
-    element: <App />
-  }
+	// Match 0, 1, or 2 parameters
+	{
+		path: "/",
+		element: <App />
+	},
+	{
+		path: "/:year",
+		element: <App />
+	},
+	{
+		path: "/:year/:month",
+		element: <App />
+	}
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 )
